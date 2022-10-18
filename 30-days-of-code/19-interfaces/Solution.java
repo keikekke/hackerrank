42d7d170e40db1bd264d71cce621e5a217379a90
Ryo Koike <ryok022366@gmail.com>
2022-10-18 19:11:37 +0900

    30days: days 17-19 done

---
+++ 30-days-of-code/19-interfaces/Solution.java
@@ -0,0 +1,38 @@
+// Because the server does not accept Go in this problem,
+// and I'm bored with Python, which I'm already familiar with,
+// so I chose Java instead for learning.
+
+import java.io.*;
+import java.util.*;
+
+interface AdvancedArithmetic{
+   int divisorSum(int n);
+}
+
+class Calculator implements AdvancedArithmetic {
+    public int divisorSum(int n) {
+        if (n == 1) return 1;
+
+        int ret = 0;
+        for (int i = 1; i*i <= n; i++) {
+            if (n % i == 0) {
+                ret += i + (n / i); // assuming i != (n/i)
+            }
+        }
+        return ret;
+    }
+}
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        scan.close();
+
+      	AdvancedArithmetic myCalculator = new Calculator();
+        int sum = myCalculator.divisorSum(n);
+        System.out.println("I implemented: " + myCalculator.getClass().getInterfaces()[0].getName() );
+        System.out.println(sum);
+    }
+}
